b5e8ddffac2c609dd3194f127f8dae51ee8b44df
Mark Tiddy <marktiddy@gmail.com>
2020-02-15 11:19:50 +0000

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,201 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <md-toolbar class="md-primary">
+      <h3 class="md-title">
+        <i class="material-icons">
+          done
+        </i>
+        Tick List
+      </h3>
+    </md-toolbar>
+    <div class="main-content">
+      <p>
+        Welcome to Tick List. Start off by adding something you want to get
+        done.
+      </p>
+      <!--new todo input code -->
+      <md-field>
+        <md-input
+          v-model="currentTodo"
+          @keydown.enter="addTodo()"
+          placeholder="Add a todo"
+        />
+      </md-field>
+      <!--Code to let users add a todo-->
+
+      <div
+        class="md-layout md-gutter md-medium-size-33 md-small-size-50 md-xsmall-size-100"
+      >
+        <div
+          class="md-layout-item todo-card"
+          v-for="todo in todos"
+          :key="todo.id"
+        >
+          <md-card md-with-hover>
+            <md-card-content>
+              <div class="grid-container">
+                <div class="grid-item_check">
+                  <md-checkbox
+                    v-model="todo.isCompleted"
+                    @click="updateCheck(todo)"
+                  ></md-checkbox>
+                </div>
+                <div class="grid-item">
+                  <span class="todo-item">{{ todo.label }}</span>
+                  <span :class="{ hide: !todo.isBeingEdited }">
+                    <md-field>
+                      <md-input
+                        v-model="updatingTodo"
+                        @keydown.enter="updateTodo(todo)"
+                        :placeholder="todo.label"
+                        :class="{ hide: !todo.isBeingEdited }"
+                      />
+                      <span
+                        class="md-caption"
+                        :class="{ hide: !todo.isBeingEdited }"
+                        >Update your item</span
+                      >
+                    </md-field></span
+                  >
+                </div>
+                <div class="grid-item">
+                  <button class="edit-button" @click="setEditing(todo)">
+                    <i class="material-icons">edit</i>
+                  </button>
+                </div>
+              </div> </md-card-content
+            ><md-card-actions md-alignment="left">
+              <md-button class="md-raised md-primary" @click="removeTodo(todo)"
+                >Delete</md-button
+              >
+            </md-card-actions></md-card
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [],
+      //a text input for adding todos
+      currentTodo: "",
+      updatingTodo: "",
+      boolCheck: false
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false,
+        isBeingEdited: false
+      });
+      this.currentTodo = "";
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    updateCheck(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos[index].completed = !todo.completed;
+    },
+    setEditing(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos[index].isBeingEdited = !this.todos[index].isBeingEdited;
+      this.updatingTodo = this.todos[index].label;
+    },
+    updateTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos[index].label = this.updatingTodo;
+      this.todos[index].isBeingEdited = !this.todos[index].isBeingEdited;
+      this.updatingtodo = "";
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.main-content {
+  padding: 20px;
+}
+
+.hide {
+  display: none;
+  visibility: hidden;
+}
+
+.grid-container {
+  display: grid;
+  grid-template-columns: 0.5fr 2fr 0.5fr;
+}
+
+.grid-item {
+  padding: 10px;
+}
+
+.grid-item_check {
+  margin-top: -5px;
+}
+
+.todos {
+  list-style: none;
+  line-height: 3em;
+}
+
+.todo-item {
+  font-size: 18px;
+}
+
+.todo-card {
+  margin-bottom: 20px;
+}
+
+.edit-button {
+  border: 0px;
+}
+.edit-button:hover {
+  opacity: 0.8;
+  cursor: pointer;
 }
 </style>
+
+<!--
+<div class="main-content">
+      <p>
+        Welcome to Tick List. Start off by adding something you want to get
+        done.
+      </p>
+      <md-field>
+        <md-input
+          v-model="currentTodo"
+          @keydown.enter="addTodo()"
+          placeholder="Add a todo"
+        />
+      </md-field>
+      <ul class="todos">
+        <li v-for="todo in todos" :key="todo.id">
+          <input type="checkbox" @click="updateCheck(todo)" />
+          <label
+            @dblclick="setEditing(todo)"
+            :class="{ hide: todo.isBeingEdited }"
+            >{{ todo.label }}</label
+          >
+          <input
+            type="text"
+            v-model="updatingTodo"
+            :placeholder="todo.label"
+            @keydown.enter="updateTodo(todo)"
+            :class="{ hide: !todo.isBeingEdited }"
+          />
+          <button @click="removeTodo(todo)">Delete</button>
+        </li>
+      </ul>
+    </div>
+
+-->
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueMaterial from "vue-material";
+import {
+  MdField,
+  MdCheckbox,
+  MdToolbar,
+  MdCard,
+  MdLayout,
+  MdElevation,
+  MdButton
+} from "vue-material/dist/components";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/black-green-light.css";
 
-Vue.config.productionTip = false
+Vue.use(VueMaterial);
+Vue.use(MdField);
+Vue.use(MdToolbar);
+Vue.use(MdCard);
+Vue.use(MdLayout);
+Vue.use(MdElevation);
+Vue.use(MdCheckbox);
+Vue.use(MdButton);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
